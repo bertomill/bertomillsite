81855f8f46b199f98eaed89c1b24dba8499d0d1a
Berto MIll <81169127+bertomill@users.noreply.github.com>
2024-07-07 12:25:14 -0400

    adjusted projects

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -5,9 +5,10 @@ import { Link } from 'react-router-dom';
 
 const projects = [
   {
-    title: 'Sift AI',
+    title: 'AI Lens',
     description: 'A web application that provides insights, analysis, and data exclusively on AI topics. It tracks how major global banks are using AI and includes news, earnings calls, annual reports, data analysis on the AI landscape, regulatory updates, and research institute updates on AI.',
-    link: '/projects/sift-ai',
+    link: 'https://ainews-beta.vercel.app/',
+    image: '/assets/img/AI Lens.png' // Add the image URL here
   },
   // Add more projects here as needed
 ];
@@ -19,9 +20,10 @@ const Projects = () => (
     <div className="project-list">
       {projects.map((project, index) => (
         <div key={index} className="project">
+          <img src={project.image} alt={`${project.title} logo`} className="project-image" />
           <h2>{project.title}</h2>
           <p>{project.description}</p>
-          <Link to={project.link} className="project-link">Learn More</Link>
+          <Link to={project.link} className="project-link">Go To Project</Link>
         </div>
       ))}
     </div>
